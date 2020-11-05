dc3cd21323f7d7c66cd178520919773f468a4faa
Yuta <y.sugiyama54@gmail.com>
2020-11-05 23:16:01 +0900

    indexファイルの修正

--- www/html/index.php
+++ www/html/index.php
@@ -1,3 +1,14 @@
-<php
-hello world
-phpinfo();
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>php7.2-apache</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<?php
+echo 'Hello world.';
+?>
+</body>
+</html>
