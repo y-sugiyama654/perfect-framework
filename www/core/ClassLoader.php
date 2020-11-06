4a56167ed149a18ffb2a43deff70d12042b46b65
Yuta <y.sugiyama54@gmail.com>
2020-11-06 23:54:16 +0900

    オートローダクラスの作成

---
+++ www/core/ClassLoader.php
@@ -0,0 +1,40 @@
+<?php
+
+class ClassLoader
+{
+    protected $dirs;
+
+    /**
+     * オートローダーロウロク処理
+     */
+    public function register()
+    {
+        mysql_autoload_register(array($this, 'loadClass'));
+    }
+
+    /**
+     * ディレクトリ登録処理
+     *
+     * @param string $dir ディレクトリ名
+     */
+    public function registerDir(string $dir)
+    {
+        $this->dirs[] = $dir;
+    }
+
+    /**
+     * クラスファイルの読み込み処理
+     *
+     * @param string $class クラス名
+     */
+    public function loadClass(string $class)
+    {
+        foreach ($this->dirs as $dir) {
+            $file = $dir . '/' . $class . '.php';
+            if (is_readable($file)) {
+                require $file;
+                return;
+            }
+        }
+    }
+}
